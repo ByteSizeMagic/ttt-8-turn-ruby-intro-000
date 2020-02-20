ded273b728e1ef5d8c0ba266318a27ac645bcb0f
Crystal Schooling <bytesizemagic@gmail.com>
2020-02-20 11:54:15 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,43 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  index = gets.strip
+  input_to_index(index)
+  if valid_move?(board, index) == true
+    move(board, index, token)
+  elsif valid_move?(board, index) == false
+    turn(board)
+  end
+  display_board(board)
+end
